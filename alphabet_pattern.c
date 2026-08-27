ea31d0309fe6dbeb3bfbc9b275c95c95d8fbfac1
Pratik Shete <pratikshete827@gmail.com>
2026-08-27 14:37:59 +0530

    Add files via upload

---
+++ alphabet_pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	char c = 'A';
+	for(i = 1; i<= 5; i++)
+	{
+		for(j = 1; j<=5; j++)
+		{
+			if(j <= i)
+			{
+				printf("%c ",c);
+				
+			}
+			else{
+				printf(" ");
+			}
+		
+		}
+		c++;
+		printf("\n");
+	}
+	return 0;
+}
